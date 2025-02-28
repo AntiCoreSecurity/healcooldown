cd4e430d382d51b5ca6425a2fc9eeb32c0bba66f
sfsdf-security <saladang1199@gmail.com>
2025-02-28 21:28:25 +0900

    Add files via upload

---
+++ src/main/java/org/sfsdf/healthcooldown/Healthcooldown.java
@@ -0,0 +1,10 @@
+package org.sfsdf.healthcooldown;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Healthcooldown implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
